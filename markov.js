aa454ceb5dba82eca658ab61c97196d5bccb275b
CrappyDill <mddillon47@gmail.com>
2021-10-01 23:02:58 -0700

    markov machine

--- markov.js
+++ markov.js
@@ -1,13 +1,11 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
+    this.words = words.filter((c) => c !== "");
     this.makeChains();
   }
 
@@ -17,13 +15,38 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let chains = new Map();
+
+    for (let i = 0; i < this.words.length; i += 1) {
+      let word = this.words[i];
+      let next = this.words[i + 1] || null;
+
+      if (chains.has(word)) chains.get(word).push(next);
+      else chains.set(word, [next]);
+    }
+
+    this.chains = chains;
   }
 
-
   /** return random text from chains */
+  static random(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
 
   makeText(numWords = 100) {
     // TODO
+    let keys = Array.from(this.chains.keys());
+    let key = MarkovMachine.random(keys);
+    let out = [];
+
+    while (out.length < numWords && key !== null) {
+      out.push(key);
+      key = MarkovMachine.random(this.chains.get(key));
+    }
+    return out.join(" ");
   }
 }
+
+module.exports = {
+  MarkovMachine,
+};
